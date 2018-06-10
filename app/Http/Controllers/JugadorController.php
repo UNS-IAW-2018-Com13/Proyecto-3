1a0757b3b682cc2841ac92c956f73e69d992d7f5
Raptor <ruben_dario2001ar@hotmail.com>
2018-06-10 16:47:17 -0300

    sad
    fsa

--- app/Http/Controllers/JugadorController.php
+++ app/Http/Controllers/JugadorController.php
@@ -5,29 +5,49 @@ namespace App\Http\Controllers;
 use App\Jugador;
 use App\Mazo;
 use App\Http\Controllers\Controller;
-
 use Illuminate\Http\Request;
 
+class JugadorController extends Controller {
 
-class JugadorController extends Controller
-{
     /**
      * Show the profile for the given user.
      *
      * @param  int  $id
      * @return Response
      */
-    
-    public function salvar(){
+    public function salvar() {
         
     }
 
+    public function show(Request $parametros) {
+        $jugador = Jugador::where('nombre', '=', $parametros->InNombre)->get();
+        if (sizeof($jugador) == 0) {
+            $jugador = new Jugador();
+            $jugador->nombre = $parametros->InNombre;
+            $jugador->puntaje = 0;
+            $jugador->idFavorito = sizeof(Jugador::all());
+            $jugador->favorito = 0;
+            $jugador->mazos = [$parametros->InNombre . "1", $parametros->InNombre . "2", $parametros->InNombre . "3"];
+            /*
+              $cards = explode(chr(13), $_POST['InDeck1']);
+              $mazo = "{'nombre'='{$_POST['InNombre']}1',
+              'clase'= '" . substr($cards[1], 10) . "',
+              'mazos'= [";
+              for ($i = 4; $i < sizeof($cards) - 5; $i++) {
+              $mazo .= "['" . substr($cards[$i], 10) . "', " . substr($cards[$i], 3, 1) . "],";
+              }
+              $mazo = substr($mazo, 0, -1) . "]}";
+              echo $mazo . "<br/>";
+             */
+        } else {
+            $jugador = json_decode("{}");
+            $jugador->msg = "existe";
+        }
 
-    public function show(Request $parametros)
-    {
-        $jugador= new Jugador();
-        $jugador->nombre = $parametros->InNombre;
-        
-        return view('res');
+
+        //$jugador->save();
+
+        return view('res', ['jugador' => $jugador]);
     }
+
 }
--- resources/views/res.blade.php
+++ resources/views/res.blade.php
@@ -1,50 +1,19 @@
-<?php
+@extends('layouts.app')
 
-$jugador = "{'nombre'='{$_POST['InNombre']}',
-                'puntaje'=0,
-                'idFavorito'=0,  
-                'favorito'=0,
-                'mazos'=[
-                    '{$_POST['InNombre']}1',
-                    '{$_POST['InNombre']}2',
-                    '{$_POST['InNombre']}3'
-                        ]}";
-echo $jugador;
-?>
-<br/>
-<br/>
-<?php
-$cards = explode(chr(13), $_POST['InDeck1']);
-$mazo = "{'nombre'='{$_POST['InNombre']}1',
-    'clase'= '" . substr($cards[1], 10) . "',
-    'mazos'= [";
-for ($i = 4; $i < sizeof($cards) - 5; $i++) {
-    $mazo .= "['" . substr($cards[$i], 10) . "', " . substr($cards[$i], 3, 1) . "],";
-}
-$mazo = substr($mazo, 0, -1) . "]}";
-echo $mazo . "<br/>";
-?>
-<br/>
-<?php
-$cards = explode(chr(13), $_POST['InDeck2']);
-$mazo = "{'nombre'='{$_POST['InNombre']}2',
-    'clase'= '" . substr($cards[1], 10) . "',
-    'mazos'= [";
-for ($i = 4; $i < sizeof($cards) - 5; $i++) {
-    $mazo .= "['" . substr($cards[$i], 10) . "', " . substr($cards[$i], 3, 1) . "],";
-}
-$mazo = substr($mazo, 0, -1) . "]}";
-echo $mazo . "<br/>";
-?>
-<br/>
-<?php
-$cards = explode(chr(13), $_POST['InDeck3']);
-$mazo = "{'nombre'='{$_POST['InNombre']}3',
-    'clase'= '" . substr($cards[1], 10) . "',
-    'mazos'= [";
-for ($i = 4; $i < sizeof($cards) - 5; $i++) {
-    $mazo .= "['" . substr($cards[$i], 10) . "', " . substr($cards[$i], 3, 1) . "],";
-}
-$mazo = substr($mazo, 0, -1) . "]}";
-echo $mazo . "<br/>";
-?>
+@section('content')
+<div class="container">
+    <?php
+    if (array_key_exists("msg", $jugador)) {
+        echo "<label>" . $jugador->msg . "</label><br/>";
+    } else {
+        echo "<label>" . $jugador->nombre . "</label><br/>
+        <label>" . $jugador->mazos[0] . "</label><br/>
+        <label>" . $jugador->mazos[1] . "</label><br/>
+        <label>" . $jugador->mazos[2] . "</label><br/><br/>";
+    }
+    ?>
+    <a href="/">
+        <button class="btn btn-primary">Volver</button>
+    </a><br/>
+</div>
+@endsection
