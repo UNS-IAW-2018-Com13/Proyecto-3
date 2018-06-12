a0ef57383f8c13313d6ef6714f506cc0ce4e3806
Alejandro-Javier-Vila <ajvb89@gmail.com>
2018-06-12 16:22:16 -0300

    asd
    asd

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Admin Torneo Hearthstone'),
+    'name' => env('APP_NAME', 'Torneo HS Admin'),
 
     /*
     |--------------------------------------------------------------------------
